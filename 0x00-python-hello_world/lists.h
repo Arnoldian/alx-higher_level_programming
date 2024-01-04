5a8cbf739447bc628d5e3816f9183a7bf9f97cb2
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-03 17:42:46 -0800

    mandatory tasks

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,6 +3,13 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly-linked list
+ * @n: int
+ * @next: points to next node
+ * Description: singly-linked list node struct
+ */
+
 typedef struct listint_s
 {
     int n;
