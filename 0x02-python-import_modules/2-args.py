56785ddb3236b1339bb3fbf865f8050ff3a354e1
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-04 17:53:11 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == "__main__":
-    i = len(sys.argv) - 1
-
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    largs = sys.argv
+    if (len(sys.argv) - 1) == 0:
+        print("0 arguments.")
+    elif (len(sys.argv) - 1) == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        print(f"{(len(sys.argv) - 1):d} arguments:")
+    for i in range(1, len(largs)):
+        print(f"{i:d}: {largs[i]}")
