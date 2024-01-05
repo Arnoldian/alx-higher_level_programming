f8886a1aae7f36d2e222ee70427d4ca1714bf98c
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-04 18:11:17 -0800

    list.h fix

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -16,6 +16,10 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 int check_cycle(list_t *list);
 
 #endif
