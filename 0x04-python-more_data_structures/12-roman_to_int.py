a61dd1867f2e1fc4a290db70ace59e5da5d57fbc
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-10 18:50:05 -0800

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,8 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
 
-    roman_dict = { 'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000 }
+    roman_dict = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, \
+            'C': 100, 'D': 500, 'M': 1000 }
 
     decimal = [roman_dict[x] for x in roman_string]
 
