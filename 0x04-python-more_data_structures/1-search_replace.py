c34a1599b976c86dfcdc7ea677e7695272ae6883
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-10 18:58:28 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    return (list(map(lambda elem: replace \
-            if elem == search else elem, my_list)))
+    return (list(map(lambda n: replace if n == search else n, my_list)))
