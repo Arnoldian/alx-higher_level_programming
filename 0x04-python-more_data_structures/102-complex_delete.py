321e93a2326d424467d632ed5dbd2da9b1c9f08d
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-10 19:02:20 -0800

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+
     for n in list(a_dictionary.keys()):
-        if a_dictionary[x] is value:
+        if a_dictionary[n] is value:
             del a_dictionary[n]
     return (a_dictionary)
