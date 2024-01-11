61a80fa13604bec2a3a1d83277e23f7f70d52437
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-10 18:47:14 -0800

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    return sum([n[0] * n[1] for n in my_list]) / (sum([n[1] for n in my_list]) or 1)
+    return sum([n[0] * n[1] for n in my_list]) \
+            / (sum([n[1] for n in my_list]) or 1)
