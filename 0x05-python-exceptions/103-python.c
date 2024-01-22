f8cdd49146441ae81aaf4963bbd5e0a7251a775c
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-22 08:43:44 -0800

    task 10 touch ups

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <ctype.h>
 
+void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_bytes - prints bytes info
  * @p: Py Obj ptr
