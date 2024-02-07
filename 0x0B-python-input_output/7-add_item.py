97ea816b7fc03e3310899a620c8557d6ce24da47
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-02-06 17:14:36 -0800

    task touch ups

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,14 @@
 Module adding all args to Py list and saving to file
 """
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+#from 5-save_to_json_file import save_to_json_file
+#from 6-load_from_json_file import load_from_json_file
 
 
 if __name__ == "__main__":
-    #save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    #load_from_json_file = \
-     #   __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
